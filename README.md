e378942617c773fcd6d6f4d20e1fc5d82e41b7b6
Ales Katona <almindor@gmail.com>
2020-03-08 14:33:09 -0600

    move shapes example into folder + readme info

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# st7789-examples
-[ST7789](https://github.com/almindor/st7789) driver examples.
+# ST7789 Examples
+[ST7789](https://github.com/almindor/st7789) driver examples for use with the [PineTime](https://wiki.pine64.org/index.php/PineTime) watch.
+
+## Usage
+
+Run `cargo run --release --example <name>` where `<name>` is the example name.
+
+### List of examples
+
+`shapes` - display all shapes supported by embedded-graphics
--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,18 @@
 #![no_std]
 #![no_main]
 
-extern crate cortex_m_rt as rt; // v0.5.x
-
+extern crate cortex_m_rt as rt;
 extern crate nrf52832_hal;
 extern crate panic_halt;
 
 use cortex_m_rt::entry;
-use embedded_graphics::prelude::*;
-use embedded_graphics::primitives::*;
-use embedded_graphics::style::*;
-use embedded_graphics::pixelcolor::Rgb565;
-use nrf52832_hal::gpio::Level;
-use nrf52832_hal::gpio::*;
-use nrf52832_hal::spim;
-use nrf52832_hal::Delay;
-use st7789::{ST7789, Orientation};
-use cortex_m_semihosting::hprintln;
+
 
 #[entry]
 fn main() -> ! {
-    let core = nrf52832_hal::nrf52832_pac::CorePeripherals::take().unwrap();
-    let delay = Delay::new(core.SYST);
-
-    let p = nrf52832_hal::nrf52832_pac::Peripherals::take().unwrap();
-    let port0 = p.P0.split();
-
-    let _backlight = port0.p0_22.into_push_pull_output(Level::Low); // set medium backlight on
-    let rst = port0.p0_26.into_push_pull_output(Level::Low); // reset pin
-    let _cs = port0.p0_25.into_push_pull_output(Level::Low); // keep low while drivign display
-    let dc = port0.p0_18.into_push_pull_output(Level::Low); // data/clock switch
-
-    let spiclk = port0.p0_02.into_push_pull_output(Level::Low).degrade(); // SPI clock to LCD
-    let spimosi = port0.p0_03.into_push_pull_output(Level::Low).degrade(); // SPI MOSI to LCD
-
-    let pins = spim::Pins {
-        sck: spiclk,
-        miso: None,
-        mosi: Some(spimosi),
-    };
-
-    // create SPI interface
-    let spi = spim::Spim::new(p.SPIM0, pins, spim::Frequency::M8, spim::MODE_3, 122);
-
-    // create driver
-    let mut display = ST7789::new(spi, dc, rst, 240, 240, delay);
-
-    // initialize
-    display.init().unwrap();
-    // set default orientation
-    display.set_orientation(&Orientation::Landscape).unwrap();
-
-    let blank = Rectangle::new(Point::new(0, 0), Point::new(239, 239)).into_styled(PrimitiveStyle::with_fill(Rgb565::BLACK));
-    let circle1 = Circle::new(Point::new(128, 64), 64).into_styled(PrimitiveStyle::with_fill(Rgb565::RED));
-    let circle2 = Circle::new(Point::new(64, 64), 64).into_styled(PrimitiveStyle::with_stroke(Rgb565::GREEN, 1));
-    let triangle = Triangle::new(Point::new(40, 100), Point::new(40, 140), Point::new(80, 120)).into_styled(PrimitiveStyle::with_fill(Rgb565::BLUE));
-
-    // draw two circles on blue background
-    blank.draw(&mut display).unwrap();
-    circle1.draw(&mut display).unwrap();
-    circle2.draw(&mut display).unwrap();
-    triangle.draw(&mut display).unwrap();
-
-    hprintln!("Rendering done").unwrap();
+    // See examples folder
 
     loop {
-        continue; // keep optimizer from removing in --release
+        continue;
     }
 }
